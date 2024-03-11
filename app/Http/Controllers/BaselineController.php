2cf63e72283beaddb3a367d30452de8b8b87ef77
itaivincent <itaivincent321@gmail.com>
2024-03-11 10:43:07 +0200

    test

--- app/Http/Controllers/BaselineController.php
+++ app/Http/Controllers/BaselineController.php
@@ -133,7 +133,7 @@ class BaselineController extends Controller
    
          foreach ($truckData as $truckCode => $rows) {
      
-         $trucks =  DB::connection('mysql')->table('baseline')->where('Truck', '=', $rows->Truck)->orderBy('Date')->orderBy('Time')->take(5)->get();
+         $trucks =  DB::connection('mysql')->table('baseline')->where('Truck', '=', $rows->Truck)->orderBy('Date')->orderBy('Time')->get();
         // dd($trucks);
 
         foreach ($trucks as $trip) {
