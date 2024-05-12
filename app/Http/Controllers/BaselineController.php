bd85d47ead0012430690a440082f81195bba86a9
itaivincent <itaivincent321@gmail.com>
2024-05-12 18:03:46 +0200

    done

--- app/Http/Controllers/BaselineController.php
+++ app/Http/Controllers/BaselineController.php
@@ -906,7 +906,7 @@ class BaselineController extends Controller
         set_time_limit(360000000000);
 
            $truckData = DB::connection('mysql')->table('baselinev2')->whereBetween('Date', ['2024-02-01' , '2024-02-31'])->groupBy('Truck')->orderBy('id')->get();
-       // dd($truckData);
+        dd($truckData);
 
          foreach ($truckData as $truckCode => $rows) {
 
